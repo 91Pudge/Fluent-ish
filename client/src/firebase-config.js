6de1897e6d76e10eaf2c05c80fc76c2e008aae8a
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-11-12 14:24:40 +0900

    Feat: Create firebase file

---
+++ client/src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIlnHF457xaik5FArYG5nsFBrtaRJN90M",
+  authDomain: "fluent-ish.firebaseapp.com",
+  projectId: "fluent-ish",
+  storageBucket: "fluent-ish.appspot.com",
+  messagingSenderId: "888969012297",
+  appId: "1:888969012297:web:16f9e902c45b4e12f64b98",
+  measurementId: "G-JRLBHEYLFW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+const analytics = getAnalytics(app);
+export default app;
