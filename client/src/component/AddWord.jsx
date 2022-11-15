6df8c417c2ebe9a4743713f0f47f808ecb8dbc3f
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-11-15 12:33:33 +0900

    Feat: Add catergories to word translation

--- client/src/component/AddWord.jsx
+++ client/src/component/AddWord.jsx
@@ -1,14 +1,48 @@
 import { InputGroup, Form, ButtonGroup, Button, Alert } from "react-bootstrap";
 import { useState } from "react";
+import WordDataService from "../WordServices";
+import { useUserAuth } from "../context/UserAuthContext";
 
-const Addword = () => {
-  const [message, setMessage] = useState();
-  const [word, setWord] = useState();
-  const [flag, setFlag] = useState();
-  const [description, setDescription] = useState();
-  const [status, setStatus] = useState();
+const Addword = (id, setWordId) => {
+  const [word, setWord] = useState("");
+  const [description, setDescription] = useState("");
+  const [translation, setTranslation] = useState("");
+  const [message, setMessage] = useState("");
 
-  const handleSubmit = async () => {};
+  const { user } = useUserAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    if (word === "" || description === "") {
+      setMessage({ error: true, msg: "All fields are mandatory" });
+      return;
+    }
+    const newWord = {
+      word,
+      description,
+      translation,
+      uid: user.uid,
+      created_at: new Date()
+    };
+    console.log(newWord);
+    try {
+      if (id !== undefined && id !== "") {
+        await WordDataService.updateWords(id, newWord);
+        setWordId("");
+        setMessage({ error: false, msg: "Updated succesfully added" });
+      } else {
+        await WordDataService.addWords(newWord);
+        setMessage({ error: false, msg: "New word succesfully added" });
+      }
+    } catch (error) {
+      console.log(error.message);
+      setMessage({ error: true, msg: error.message });
+    }
+    setWord("");
+    setDescription("");
+    setTranslation("");
+  };
 
   return (
     <>
@@ -44,6 +78,17 @@ const Addword = () => {
                 placeholder="Description"
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
+              />
+            </InputGroup>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formDescription">
+            <InputGroup>
+              <InputGroup.Text id="formDescriptionr">{">"}</InputGroup.Text>
+              <Form.Control
+                type="text"
+                placeholder="Translation"
+                value={translation}
+                onChange={(e) => setTranslation(e.target.value)}
               />
             </InputGroup>
           </Form.Group>
